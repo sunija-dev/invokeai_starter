dac7977e435f935a12c214465ef33369aad83dfb
Sunija <gothab@t-online.de>
2023-02-18 04:45:13 +0100

    initial commit, basic ui layout

---
+++ invokeai_starter/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace invokeai_starter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // if first start: create desktop shortcut
+
+            // check that license is accepted
+            // -> remove license text + change button text + button action
+
+            // set output folder
+
+            // set links + copy button actions
+
+            // check if system fulfills requirements (gpu, ram, no spaces, special characters, brackets)
+        }
+    }
+}
